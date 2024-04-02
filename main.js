85c43a9b59f49e71936b2ca7221463cd5d74ceb4
ravitoria <128604566+ravitoria@users.noreply.github.com>
2024-04-02 09:44:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const botoes = document.querySelectorAll(".botao");
-
+console.log(botoes)
+const textos = document.querySelectorAll(".aba-conteudo");
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
 
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
+for(let i=0; i <  botoes.length; i++){
+    console.log(i);
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
